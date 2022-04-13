e84eca4f6e074b3ba6fb12d0a301d04c28303de6
matankorenfeld <m4t4n04@gmail.com>
2022-04-13 15:07:06 +0300

    fix Code style

--- src/main/java/com/example/garagemanagement/GarageManagementApplication.java
+++ src/main/java/com/example/garagemanagement/GarageManagementApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GarageManagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GarageManagementApplication.class, args);
     }
-
 }
